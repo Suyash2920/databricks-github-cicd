646ffa7f4685ffbc4458bab3a937ceb784c4f260
Gajendra Goswami <59818944+Suyash2920@users.noreply.github.com>
2026-09-22 20:20:45 +0530

    Update main.py

--- notebooks/main.py
+++ notebooks/main.py
@@ -1,18 +1,20 @@
 # Databricks notebook source
 
+import sys
+
+sys.path.append("/Shared/github-cicd")
+
+
 from src.transformations import transform_data
 from src.transformations import calculate_total
 
 
-# Sample input data
 data = [10, 20, 30, 40, 50]
 
 
-# Transform data
 transformed_data = transform_data(data)
 
 
-# Calculate total
 total = calculate_total(transformed_data)
 
 
@@ -21,9 +23,13 @@ print("Databricks CI/CD Demo")
 print("====================================")
 
 print(f"Input data       : {data}")
+
 print(f"Transformed data : {transformed_data}")
+
 print(f"Total            : {total}")
 
 print("====================================")
+
 print("Execution completed successfully")
+
 print("====================================")
